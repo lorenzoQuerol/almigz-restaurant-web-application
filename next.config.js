d4ae0f2f79a5722075406d8182a67b1017938ce5
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-23 17:30:26 +0800

    MongoDB Middleware Implementation
    - installed Mongoose
    - added connection string(MONGODB_URI) in process.env (found in next.config.js)
    - implemented function to connect to MongoDB server

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        MONGODB_URI:
+            'mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/test?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    },
+};
