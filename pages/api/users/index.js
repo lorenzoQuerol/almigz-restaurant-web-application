8707142bc3d919f17dda77cc0d2b9f5714619cec
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-25 16:17:52 +0800

    Update user API routes

---
+++ pages/api/users/index.js
@@ -0,0 +1,38 @@
+import createConnection from '@utils/createConnection';
+import User from '@models/UserModel';
+
+async function handler(req, res) {
+  // Create connection to database
+  await createConnection();
+
+  // Unpack the request to get method
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const users = await User.find({});
+
+        res.status(200).json({ success: true, data: users });
+      } catch (err) {
+        res.status(400).json({ success: false, msg: err });
+      }
+      break;
+
+    case 'POST':
+      try {
+        const user = await User.create(req.body);
+
+        res.status(201).json({ success: true, data: user });
+      } catch (err) {
+        res.status(400).json({ success: false, msg: err });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
+
+export default handler;
