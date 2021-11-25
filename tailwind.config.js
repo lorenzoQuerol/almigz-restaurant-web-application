23f29f5c83a0e7607479bd4ff29241e38e2bf0c2
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-25 19:49:27 +0800

    Changed configuration for Tailwind CSS
    - changed configuration for Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     mode: 'jit',
-    purge: [
-        './public/**/*.html',
-        './src/**/*.{js,jsx,ts,tsx,vue}',
-      ],
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
