2018c8c2cf81b36fb920350b5692a84d37aebb7c
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-25 19:42:38 +0800

    Update tailwind.config.js
    - Enabled JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    mode: 'jit',
+    purge: [
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+      ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
