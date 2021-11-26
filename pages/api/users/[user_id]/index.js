360bc542f04e82699298d43d04fb6fea3d9e55ef
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-26 11:45:40 +0800

    Update index.js

--- pages/api/users/[user_id]/index.js
+++ pages/api/users/[user_id]/index.js
@@ -1,61 +1,62 @@
-import createConnection from '@utils/createConnection';
-import User from '@models/UserModel';
+import createConnection from "@utils/createConnection";
+import User from "@models/UserModel";
 
 async function handler(req, res) {
-  // Create connection to database
-  await createConnection();
+    // Create connection to database
+    await createConnection();
 
-  // Unpack the request
-  const {
-    query: { user_id },
-    method,
-  } = req;
+    // Unpack the request
+    const {
+        query: { user_id },
+        method,
+    } = req;
 
-  switch (method) {
-    // Update user
-    case 'PUT':
-      try {
-        const user = await User.findByIdAndUpdate(user_id, req.body, {
-          new: true,
-          runValidators: true,
-        });
+    switch (method) {
+        // Update user
+        case "PUT":
+            try {
+                const user = await User.findByIdAndUpdate(user_id, req.body, {
+                    new: true,
+                    runValidators: true,
+                });
 
-        if (!user) return res.status(404).json({ success: false });
+                if (!user) return res.status(404).json({ success: false });
 
-        res.status(200).json({ success: true, data: user });
-      } catch (err) {
-        res.status(400).json({ success: false, msg: err });
-      }
-      break;
+                res.status(200).json({ success: true, data: user });
+            } catch (err) {
+                res.status(400).json({ success: false, msg: err });
+            }
+            break;
 
-    // Get user
-    case 'GET':
-      try {
-        const user = await User.findById(user_id);
-        if (!user) return res.status(404).json({ success: false });
+        // Get user
+        case "GET":
+            try {
+                const user = await User.findById(user_id);
+                if (!user) return res.status(404).json({ success: false });
 
-        res.status(200).json({ success: true, data: user });
-      } catch (err) {
-        res.status(400).json({ success: false, msg: err });
-      }
-      break;
+                res.status(200).json({ success: true, data: user });
+            } catch (err) {
+                res.status(400).json({ success: false, msg: err });
+            }
+            break;
 
-    // Delete user
-    case 'DELETE':
-      try {
-        const deletedUser = await User.deleteOne({ _id: user_id });
-        if (!deletedUser) return res.status(404).json({ success: false });
+        // Delete user
+        case "DELETE":
+            try {
+                const deletedUser = await User.deleteOne({ _id: user_id });
+                if (!deletedUser)
+                    return res.status(404).json({ success: false });
 
-        res.status(200).json({ success: true, data: deletedUser });
-      } catch (err) {
-        res.status(400).json({ success: false, msg: err });
-      }
-      break;
+                res.status(200).json({ success: true, data: deletedUser });
+            } catch (err) {
+                res.status(400).json({ success: false, msg: err });
+            }
+            break;
 
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
 }
 
 export default handler;
