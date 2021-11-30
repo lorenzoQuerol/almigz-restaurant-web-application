747bb62e4114e6a8ef3d62b94d0d739095e35529
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-11-30 13:22:56 +0800

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,55 @@
 import mongoose from "mongoose";
+import Double from "@mongoosejs/double";
+
+const ImageSchema = new mongoose.Schema({
+    imgUrl: {
+        type: String,
+        required: false,
+    },
+});
+
+// Schema is modelled after the content model from the Contentful headless CMS
+const MenuItemSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: false,
+    },
+
+    category: {
+        type: String,
+        required: false,
+    },
+
+    description: {
+        type: String,
+        required: false,
+    },
+
+    price: {
+        type: Double,
+        required: false,
+    },
+
+    isAvailable: {
+        type: Boolean,
+        required: false,
+    },
+
+    // Array of images (in case there are multiple images)
+    imgUrls: [ImageSchema],
+});
+
+const CartItemSchema = new mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: false,
+    },
+
+    menuItem: {
+        type: MenuItemSchema,
+        required: false,
+    },
+});
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -35,6 +86,8 @@ const UserSchema = new mongoose.Schema({
         type: Number,
         required: false,
     },
+
+    cart: { type: [CartItemSchema], required: false, default: [] },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
