41d438c25d7df2c1919ded70a094e439a4f1670b
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 00:33:21 +0800

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,33 +1,29 @@
-import Link from 'next/link';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Navbar = () => {
-  return (
-    <nav>
-    	<div class="">
-			<div class="">
-                <Link href="/">
-				<div class="">
-                <Image src="/logo.png" alt="store-logo" width={200} height={50}/>
-				</div>
-                </Link>
+    return (
+        <nav>
+            <div className="">
+                <div className="">
+                    <Link href="/">
+                        <div className="">
+                            <Image src="/logo.png" alt="store-logo" width={200} height={50} />
+                        </div>
+                    </Link>
 
-				<Link href="/">
-                <a class="">
-					HOME
-				</a>
-                </Link>
-                
-				<Link href="/menu">
-                <a class="">
-					MENU
-				</a>
-                </Link>
-			</div>
-		</div>
-    </nav>
-	);
-}
- 
+                    <Link href="/">
+                        <a className="">HOME</a>
+                    </Link>
+
+                    <Link href="/menu">
+                        <a className="">MENU</a>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
 export default Navbar;
