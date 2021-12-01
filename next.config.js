11deab032368dddc335dab09434e4b01816d7a35
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 00:32:32 +0800

    Update next.config.js
    - Added URLS

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGODB_URI:
-      'mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/test?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
-    CONTENTFUL_SPACE_ID: '4vhelr5l3xmh',
-    CONTENTFUL_ACCESS_TOKEN: 'zOgM8Qj5VXL9pjyqYuhbFvxvJE0vQzZRL_2Wt7uADZs',
-    CONTENTFUL_PREVIEW_ACCESS_TOKEN: 'FeIT2KtX3Sy2GfGj2-b2q4yDISjkABqGTrMl5IqKpJw',
-  },
+    reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_URL: "http://localhost:3000",
+        NEXTAUTH_URL: "http://localhost:3000",
+        MONGODB_URI:
+            "mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/test?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+        CONTENTFUL_SPACE_ID: "4vhelr5l3xmh",
+        CONTENTFUL_ACCESS_TOKEN: "zOgM8Qj5VXL9pjyqYuhbFvxvJE0vQzZRL_2Wt7uADZs",
+        CONTENTFUL_PREVIEW_ACCESS_TOKEN: "FeIT2KtX3Sy2GfGj2-b2q4yDISjkABqGTrMl5IqKpJw",
+    },
 };
