c885d2ffe40bccb230dd500ed118e13de9a676ec
gretvm <80465081+gretvm@users.noreply.github.com>
2021-12-02 07:28:52 +0800

    Added designed to NavBar
    Added design and nav to login and register pages
    To be edited: alignment and hiding of login and register button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,27 +4,39 @@ import styles from '../styles/Home.module.css'
 
 const Navbar = () => {
   return (
-    <nav>
-    	<div class="">
-			<div class="">
+    <nav class="navbar h-24 mb-2 shadow-lg text-neutral-content rounded-box border-t-8 border-green-800">
+    	<div class="flex items-center">
+			<div class="lg:flex self-center">
                 <Link href="/">
-				<div class="">
+				<div class="m-4">
                 <Image src="/logo.png" alt="store-logo" width={200} height={50}/>
 				</div>
                 </Link>
 
 				<Link href="/">
-                <a class="">
+                <a class="font-normal text-lg rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
 					HOME
 				</a>
                 </Link>
                 
 				<Link href="/menu">
-                <a class="">
+                <a class="font-normal text-lg rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
 					MENU
 				</a>
                 </Link>
 			</div>
+		</div>
+		<div class="">
+				<Link href="/login">
+                <a class="font-normal text-lg rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+					LOGIN
+				</a>
+                </Link>
+				<Link href="/register">
+                <a class="font-normal text-lg rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+					REGISTER
+				</a>
+                </Link>
 		</div>
     </nav>
 	);
