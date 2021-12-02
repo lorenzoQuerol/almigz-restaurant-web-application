ef841f431b88868e01ab53a50cafd6881721ec5d
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 21:04:21 +0800

    Update signIn.jsx

--- pages/auth/signIn.jsx
+++ pages/auth/signIn.jsx
@@ -35,34 +35,34 @@ export default function signInPage() {
     };
 
     return (
-        <>
-            {
-                // Error message will only render if message is defined
-                message && <h1>{message}</h1>
-            }
-            <h1>Login</h1>
-            <div className="form-control">
-                <form onSubmit={submitLogin}>
-                    <label className="label">Email Address</label>
+        <div class="w-1/2 flex-col text-center">
+            <h1 class="text-5xl font-rale text-black font-bold p-8">Login</h1>
+            <div class="rounded-md shadow-xl border-t border-t-gray-100 p-10">
+                <form class="font-rale font-lg text-gray-800 font-bold" onSubmit={submitLogin}>
+                    <label class="label">Email Address</label>
                     <input
-                        className="input input-bordered"
+                        class="input p-5  input-sm input-bordered rounded-md w-full focus:ring-2 focus:ring-blue-300"
                         type="email"
                         name="email"
+                        placeholder="Email Address"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     ></input>
 
-                    <label className="label">Password</label>
+                    <label class="label mt-4">Password</label>
                     <input
-                        className="input input-bordered"
+                        class="input p-5  input-sm input-bordered rounded-md w-full focus:ring-2 focus:ring-blue-300"
                         type="text"
                         name="password"
+                        placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     ></input>
-                    <button className="btn">Submit</button>
+                    <button class="font-normal text-white rounded-lg m-5 p-4 pl-7 pr-7 bg-green-500 hover:font-medium hover:bg-green-300">
+                        Submit
+                    </button>
                 </form>
             </div>
-        </>
+        </div>
     );
 }
