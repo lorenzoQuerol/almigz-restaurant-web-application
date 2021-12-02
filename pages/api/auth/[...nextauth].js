b458bba68e7a8f936db45245d9208cbd1101b15e
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 20:49:39 +0800

    Update [...nextauth].js
    - Added user authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,40 +1,47 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "axios";
 import { compare } from "bcryptjs";
 
 export default NextAuth({
-    // Configure JWT
-    session: {
-        jwt: true,
-    },
+    // Random string for hashing tokens
+    secret: process.env.SECRET,
 
-    pages: {
-        signIn: "/auth/login",
-        //     signOut: "/auth/signout",
-        //     error: "/auth/error", // Error code passed in query string as ?error=
-        //     verifyRequest: "/auth/verify-request", // (used for check email message)
-        //     newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+    // Configure JWT
+    session: { strategy: "jwt" },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        maxAge: 28800, // 8 hours
     },
 
     // Specify Provider
     providers: [
-        Providers.Credentials({
+        CredentialsProvider({
             async authorize(credentials) {
                 // Unpack the credentials
                 const { email, password } = credentials;
 
                 // Get user via email
-                const res = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/users/${email}`);
-                const user = res.data;
-                if (!user) throw new Error("User not found.");
+                try {
+                    const res = await axios.get(`${process.env.NEXTAUTH_URL}/api/users/${email}`);
+                    let user = res.data.data;
+                    if (!user) return null;
 
-                // Check hashed password with database password
-                const checkPassword = await compare(password, user.data.password);
-                if (!checkPassword) throw new Error("Incorrect password.");
+                    // Check hashed password with database password
+                    const checkPassword = await compare(password, user.password);
+                    if (!checkPassword) return null;
 
-                return { email: user.data.email };
+                    // Return user object
+                    user = { name: `${user.firstName} ${user.lastName}`, email: user.email };
+                    return user;
+                } catch (err) {
+                    return null;
+                }
             },
         }),
     ],
+
+    pages: {
+        signIn: "/auth/signIn",
+    },
 });
