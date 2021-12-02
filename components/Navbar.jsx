5f0a5c04554cb59aacbfa4c6baeccd8021e84499
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 21:13:51 +0800

    Changed the route for Sign In
    - Changed the route for LOGIN

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,36 +4,36 @@ import styles from "../styles/Home.module.css";
 
 const Navbar = () => {
     return (
-        <nav class="navbar h-24 mb-2 shadow-lg text-neutral-content rounded-box border-t-8 border-green-800">
-            <div class="flex items-center">
-                <div class="lg:flex self-center text-base ">
+        <nav className="h-24 mb-2 border-t-8 border-green-800 shadow-lg navbar text-neutral-content rounded-box">
+            <div className="flex items-center">
+                <div className="self-center text-base lg:flex ">
                     <Link href="/">
-                        <div class="m-4">
+                        <div className="m-4">
                             <Image src="/logo.png" alt="store-logo" width={200} height={50} />
                         </div>
                     </Link>
 
                     <Link href="/">
-                        <a class="font-normal rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+                        <a className="self-center m-5 font-normal rounded-btn hover:font-medium hover:text-green-700">
                             HOME
                         </a>
                     </Link>
 
                     <Link href="/menu">
-                        <a class="font-normal rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+                        <a className="self-center m-5 font-normal rounded-btn hover:font-medium hover:text-green-700">
                             MENU
                         </a>
                     </Link>
                 </div>
             </div>
-            <div class="">
-                <Link href="/login">
-                    <a class="font-normal rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+            <div className="">
+                <Link href="/auth/signIn">
+                    <a className="self-center m-5 font-normal rounded-btn hover:font-medium hover:text-green-700">
                         LOGIN
                     </a>
                 </Link>
                 <Link href="/register">
-                    <a class="font-normal rounded-btn self-center m-5 hover:font-medium hover:text-green-700">
+                    <a className="self-center m-5 font-normal rounded-btn hover:font-medium hover:text-green-700">
                         REGISTER
                     </a>
                 </Link>
