3bc670356a2c3bbacbeeade08edf245d6b1b6604
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 20:44:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     reactStrictMode: true,
     env: {
-        NEXT_PUBLIC_URL: "http://localhost:3000",
         NEXTAUTH_URL: "http://localhost:3000",
+        SECRET: "koInOATRiQziM5VZgcyHZIh9jMJ5VrJjDHBg+XOHl0I=",
+        JWT_SECRET: "d81046d88b4f17f80c9e6a36ba3e53d6",
         MONGODB_URI:
             "mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/test?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
         CONTENTFUL_SPACE_ID: "4vhelr5l3xmh",
