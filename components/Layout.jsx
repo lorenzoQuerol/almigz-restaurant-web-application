15d9e0666fec754f1230da4818888db8bd20d2ef
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-02 21:13:54 +0800

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,21 @@
-import Head from 'next/head'
-import Footer from "./Footer"
-import Navbar from "./Navbar"
-
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-  return (
-    <html data-theme="lofi">
-    <div>
-      <Head>
-        <title>Al Migz Special Binalot</title>
-        <link rel="icon" href="/logo.ico" />
-      </Head>
-      <Navbar />
-      <div class="w-screen min-h-screen flex justify-center">
-      { children }
-      </div>
-      <Footer />
-    </div>
-    </html>
-  );
-}
- 
+    return (
+        <html data-theme="lofi">
+            <div>
+                <Head>
+                    <title>Al Migz Special Binalot</title>
+                    <link rel="icon" href="/logo.ico" />
+                </Head>
+                <Navbar />
+                <div className="flex justify-center w-screen min-h-screen">{children}</div>
+                <Footer />
+            </div>
+        </html>
+    );
+};
+
 export default Layout;
