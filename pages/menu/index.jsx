93e51eabe28b4554d7cc918c4dae310e717a0eae
gretvm <80465081+gretvm@users.noreply.github.com>
2021-12-17 09:06:36 +0800

    Removed menu folder (images)
    Menu images now fetched from contentful

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -3,6 +3,8 @@
 // import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 import useSWR from 'swr';
+import Link from "next/link";
+
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 // console.log(fetcher);
@@ -38,11 +40,19 @@ export default function MenuPage() {
 
     return (
         <>
-            <div className="w-full">
-                <h1>MENU PAGE</h1>
+            <div className="w-full text-center">
+                <h1 className="font-bold">Binalot</h1>
+                <Link href="/menu/pork-binagoongan">Pork Binagoongan</Link><br/>
+                <Link href="/menu/al-migz-sisig">Al Migz Sisig</Link><br/>
+                <h1 className="font-bold">House Specialties</h1>
+                <Link href="/menu/sizzling-sisig">Sizzling Sisig</Link><br/>
+                <h1 className="font-bold">Pancit in Boxes</h1>
+                <Link href="/menu/malabon-small">Malabon (Small)</Link><br/>
+                <Link href="/menu/malabon-medium">Malabon (Medium)</Link><br/>
+                <h1 className="font-bold">Desserts and Refreshments</h1>
+                <Link href="/menu/coke-1-5l">Coke 1.5L</Link><br/>
             </div>
-            Not signed in <br />
-            <button onClick={signIn}>Sign in</button>
+            
         </>
     );
 }
