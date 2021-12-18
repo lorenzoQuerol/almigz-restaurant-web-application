bec4a7db9fe18936ec45741a37150a01c54413c1
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-12-18 23:52:29 +0800

    Implemented API route for homepage items

---
+++ pages/api/homepage.js
@@ -0,0 +1,44 @@
+import connectToContentful from "@utils/contentfulConnection";
+
+const query = `
+    {
+      homepageAnnouncementCollection {
+        items {
+          label
+          announcementsCollection {
+            items {
+              url
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+`;
+
+async function handler(req, res) {
+	const { method } = req;
+
+	switch (method) {
+		// Get all home page items
+		case "GET":
+			try {
+				const response = await connectToContentful(query);
+				if (!response) res.status(404).json({ success: false, msg: "Cannot find home page items." });
+
+				const homepageItems = response.data.homepageAnnouncementCollection.items[0].announcementsCollection.items;
+
+				res.status(200).json({ success: true, data: homepageItems });
+			} catch (err) {
+				res.status(400).json({ success: false, msg: err });
+			}
+			break;
+
+		default:
+			res.status(500).json({ success: false, msg: "Route is not valid." });
+			break;
+	}
+}
+
+export default handler;
