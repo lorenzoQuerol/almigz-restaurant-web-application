f15aba98e4241b199a605907a24197f468f1162f
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-02 20:47:01 +0800

    Custom made error messages in response objects

--- pages/api/cart/[email].js
+++ pages/api/cart/[email].js
@@ -2,51 +2,51 @@ import createConnection from "@utils/mongoDBConnection";
 import User from "@models/UserModel";
 
 async function handler(req, res) {
-    // Create connection to database
-    await createConnection();
+	// Create connection to database
+	await createConnection();
 
-    // Unpack the request
-    const {
-        query: { email },
-        method,
-    } = req;
+	// Unpack the request
+	const {
+		query: { email },
+		method,
+	} = req;
 
-    switch (method) {
-        // Update cart
-        case "PUT":
-            try {
-                const cart = await User.findOneAndUpdate({ email: email }, req.body, {
-                    new: true,
-                    runValidators: true,
-                });
-                if (!cart) return res.status(404).json({ success: false, msg: "User not found." });
+	switch (method) {
+		// Update cart
+		case "PUT":
+			try {
+				const cart = await User.findOneAndUpdate({ email: email }, req.body, {
+					new: true,
+					runValidators: true,
+				});
+				if (!cart) return res.status(404).json({ success: false, msg: "User not found." });
 
-                res.status(200).json({
-                    success: true,
-                    msg: "Cart updated successfully.",
-                    data: cart,
-                });
-            } catch (err) {
-                res.status(400).json({ success: false, msg: err });
-            }
-            break;
+				res.status(200).json({
+					success: true,
+					msg: "Cart updated successfully.",
+					data: cart,
+				});
+			} catch (err) {
+				res.status(400).json({ success: false, msg: err });
+			}
+			break;
 
-        // Get cart
-        case "GET":
-            try {
-                const cart = await User.findOne({ email: email }, "cart");
-                if (!cart) return res.status(404).json({ success: false, msg: "User not found." });
+		// Get cart
+		case "GET":
+			try {
+				const cart = await User.findOne({ email: email }, "cart");
+				if (!cart) return res.status(404).json({ success: false, msg: "User not found." });
 
-                res.status(200).json({ success: true, data: cart });
-            } catch (err) {
-                res.status(400).json({ success: false, msg: err });
-            }
-            break;
+				res.status(200).json({ success: true, data: cart });
+			} catch (err) {
+				res.status(400).json({ success: false, msg: `Error getting cart: ${err.message}` });
+			}
+			break;
 
-        default:
-            res.status(500).json({ success: false, msg: "Route is not valid." });
-            break;
-    }
+		default:
+			res.status(500).json({ success: false, msg: "Route is not valid." });
+			break;
+	}
 }
 
 export default handler;
