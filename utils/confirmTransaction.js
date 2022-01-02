ade60dfc4c139364f7ae8e7df7ef1d0040dae0a7
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-02 20:46:23 +0800

    Update confirmTransaction.js

--- utils/confirmTransaction.js
+++ utils/confirmTransaction.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default async function confirmTransaction(newTransaction) {
-    try {
-        // Pre-process cart for transaction
-        newTransaction.order.data.forEach((item) => {
-            delete item.menuItem.category;
-            delete item.menuItem.productDescription;
-            delete item.menuItem.isAvailable;
-            delete item.menuItem.productImagesCollection;
-            delete item.menuItem.slug;
-            delete item.menuItem.available;
-        });
+	try {
+		// Pre-process cart for transaction
+		newTransaction.order.forEach((item) => {
+			delete item.menuItem.category;
+			delete item.menuItem.productDescription;
+			delete item.menuItem.isAvailable;
+			delete item.menuItem.productImagesCollection;
+			delete item.menuItem.slug;
+			delete item.menuItem.available;
+		});
 
-        // Set to null for fields not required in DELIVERY type
-        if (newTransaction.type === 'Delivery') {
-            newTransaction.storeLocation = null;
-            newTransaction.pickupTime = null;
-        }
+		// Set to null for fields not required in DELIVERY type
+		if (newTransaction.type === "Delivery") {
+			newTransaction.storeLocation = null;
+			newTransaction.pickupTime = null;
+		}
 
-        // Set to null for fields not required in PICKUP type
-        if (newTransaction.type === 'Pickup') {
-            newTransaction.address = null;
-            newTransaction.payMethod = null;
-            newTransaction.change = null;
-            newTransaction.deliverTime = null;
-        }
+		// Set to null for fields not required in PICKUP type
+		if (newTransaction.type === "Pickup") {
+			newTransaction.address = null;
+			newTransaction.payMethod = null;
+			newTransaction.change = null;
+			newTransaction.deliverTime = null;
+		}
 
-        console.log(newTransaction);
-        const response = await axios.post(`${process.env.NEXTAUTH_URL}/api/transactions`, newTransaction);
-        const success = { success: response.data.success, msg: response.data.msg };
-        return success;
-    } catch (err) {
-        const error = { success: err.response.data.success, msg: err.response.data.msg };
-        return error;
-    }
+		const response = await axios.post(`${process.env.NEXTAUTH_URL}/api/transactions`, newTransaction);
+		delete response.data.data;
+		const success = response.data;
+		return success;
+	} catch (err) {
+		const error = err.response.data;
+		return error;
+	}
 }
