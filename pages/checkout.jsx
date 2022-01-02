3968b0935a3b95df792b7143ba74c9ec2707efae
lorenzoQuerol <renzo_querol@dlsu.edu.ph>
2022-01-02 15:28:24 +0800

    Update checkout.jsx

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,123 +1,95 @@
-import { useState, useMemo } from "react";
-import { useSession } from "next-auth/react";
-import useSWR from "swr";
+import { useState, useEffect, useMemo } from 'react';
+import { useSession } from 'next-auth/react';
+import useSWR from 'swr';
 
-import getCart from "@utils/foodCart/getCart";
-import confirmTransaction from "@utils/confirmTransaction";
+import getCart from '@utils/foodCart/getCart';
+import confirmTransaction from '@utils/confirmTransaction';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function CheckoutPage() {
-	// Session
-	const { data: session, status } = useSession();
+    // Session
+    const { data: session, status } = useSession();
 
-	// User
-	const [user, setUser] = useState();
+    // User
+    const [user, setUser] = useState();
 
-	// TEMPORARY - delivery fee
-	const delFee = 50;
+    // TEMPORARY - delivery fee
+    const delFee = 50;
 
-	// Additional details
-	const [type, setType] = useState("Delivery");
-	const [fullName, setFullName] = useState("");
-	const [email, setEmail] = useState("");
-	const [contactNum, setContactNum] = useState([]);
-	const [order, setOrder] = useState([]);
-	const [specialInstructions, setSpecialInstructions] = useState("");
-	const [totalPrice, setTotalPrice] = useState(0);
+    // Additional details
+    const [type, setType] = useState('Delivery');
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [contactNum, setContactNum] = useState([]);
+    const [order, setOrder] = useState([]);
+    const [specialInstructions, setSpecialInstructions] = useState('');
+    const [totalPrice, setTotalPrice] = useState(0);
 
-	// Delivery details
-	const [address, setAddress] = useState("");
-	const [payMethod, setPayMethod] = useState("");
-	const [change, setChange] = useState(0);
-	const [deliverTime, setDeliverTime] = useState("Now");
+    // Delivery details
+    const [address, setAddress] = useState('');
+    const [payMethod, setPayMethod] = useState('');
+    const [change, setChange] = useState(0);
+    const [deliverTime, setDeliverTime] = useState('Now');
 
-	// Pickup details
-	const [storeLocation, setStoreLocation] = useState("");
-	const [pickupTime, setPickupTime] = useState("Now");
+    // Pickup details
+    const [storeLocation, setStoreLocation] = useState('');
+    const [pickupTime, setPickupTime] = useState('Now');
 
-	// Get session email
-	useMemo(() => {
-		if (session) setEmail(session.user.email);
-	}, [session]);
+    const { data, error } = useSWR(`/api/users/${email}`, fetcher);
+    const cart = getCart();
 
-	// Fetch user details via SWR
-	const { data, error } = useSWR(`/api/users/${email}`, fetcher);
+    useEffect(() => {
+        if (session) setEmail(session.user.email);
+        if (data) setUser(data.data);
+        if (cart) setOrder(cart);
+        if (user) {
+            setFullName(`${user.firstName} ${user.lastName}`);
+            setEmail(user.email);
+            setContactNum([user.contactNum, user.altContactNum]);
+            setAddress(user.homeAddress);
+            setTotalPrice(order.total + delFee);
 
-	// Initialize form values
-	useMemo(() => {
-		const cart = getCart();
-		setOrder(cart);
+            // TEMPORARY - test transaction
+            setPayMethod('GCash');
+            setChange(1000);
+            setDeliverTime('Now');
+        }
+    }, [session, email, data, user]);
 
-		if (data) setUser(data.data);
-		if (user && cart) {
-			setFullName(`${user.firstName} ${user.lastName}`);
-			setEmail(user.email);
-			setContactNum([user.contactNum, user.altContactNum]);
-			setAddress(user.homeAddress);
-			setTotalPrice(order.total + delFee);
+    // Submit transaction
+    const submitTransaction = async (event) => {
+        event.preventDefault();
 
-			// TEMPORARY - test transaction
-			// setPayMethod("GCash");
-			// setChange(1000);
-			// setDeliverTime("Now");
-		}
-	}, [data, user]);
+        // Initialize transaction object
+        const transaction = {
+            orderStatus: 0,
+            type: type,
+            fullName: fullName,
+            email: email,
+            contactNum: contactNum,
+            order: order,
+            specialInstructions: specialInstructions,
+            totalPrice: totalPrice,
+            address: address,
+            payMethod: payMethod,
+            change: change,
+            deliverTime: deliverTime,
+            storeLocation: storeLocation,
+            pickupTime: pickupTime,
+        };
 
-	// Submit transaction
-	const submitTransaction = async (event) => {
-		event.preventDefault();
+        // Send transaction object
+        const response = await confirmTransaction(transaction);
+        console.log(response);
+    };
 
-		// Pre-process cart for transaction
-		order.data.forEach((item) => {
-			delete item.menuItem.category;
-			delete item.menuItem.description;
-			delete item.menuItem.isAvailable;
-			delete item.menuItem.imgUrls;
-		});
-
-		// Initialize transaction object
-		const transaction = {
-			orderStatus: "Confirmed",
-			type: type,
-			fullName: null,
-			email: email,
-			contactNum: contactNum,
-			order: order.data,
-			specialInstructions: specialInstructions,
-			totalPrice: totalPrice,
-			address: address,
-			payMethod: payMethod,
-			change: change,
-			deliverTime: deliverTime,
-			storeLocation: storeLocation,
-			pickupTime: pickupTime,
-		};
-
-		// Set to null for fields not required in DELIVERY type
-		if (type === "Delivery") {
-			transaction.storeLocation = null;
-			transaction.pickupTime = null;
-		}
-
-		// Set to null for fields not required in PICKUP type
-		if (type === "Pickup") {
-			transaction.address = null;
-			transaction.payMethod = null;
-			transaction.change = null;
-			transaction.deliverTime = null;
-		}
-
-		// Send transaction object
-		const response = await confirmTransaction(transaction);
-	};
-
-	return (
-		<>
-			CHECKOUT: <br />
-			<form onSubmit={submitTransaction}>
-				<button>Place Order (Open Receipt)</button>
-			</form>
-		</>
-	);
+    return (
+        <>
+            CHECKOUT: <br />
+            <form onSubmit={submitTransaction}>
+                <button>Place Order (Open Receipt)</button>
+            </form>
+        </>
+    );
 }
