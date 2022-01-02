adf3c6b90517d8e86cef9a3134d4a5b7f3586b7d
lorenzoQuerol <renzo_querol@dlsu.edu.ph>
2022-01-02 15:26:55 +0800

    Added an error handler for user and transaction

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,48 +1,55 @@
-import createConnection from "@utils/mongoDBConnection";
-import Transaction from "@models/TransactionModel";
+import createConnection from '@utils/mongoDBConnection';
+import Transaction from '@models/TransactionModel';
+import transactionErrorHandler from '@handlers/transactionErrorHandler';
 
 async function handler(req, res) {
-	// Create connection to database
-	await createConnection();
+    // Create connection to database
+    await createConnection();
 
-	// Unpack the request
-	const {
-		query: { email },
-		method,
-	} = req;
+    // Unpack the request
+    const {
+        query: { email },
+        method,
+    } = req;
 
-	switch (method) {
-		// Get all transactions
-		case "GET":
-			try {
-				const transactions = await Transaction.find({}, { __v: false });
-				if (!transactions) return res.status(404).json({ success: false, msg: "Transactions cannot be found." });
+    switch (method) {
+        // Get all transactions
+        case 'GET':
+            try {
+                const transactions = await Transaction.find({}, { __v: false });
+                if (!transactions)
+                    return res.status(404).json({ success: false, msg: 'Transactions cannot be found.' });
 
-				res.status(200).json({ success: true, data: transactions });
-			} catch (err) {
-				res.status(400).json({ success: false, msg: err });
-			}
-			break;
+                res.status(200).json({ success: true, data: transactions });
+            } catch (err) {
+                res.status(400).json({ success: false, msg: `Error getting transactions: ${err.message}` });
+            }
+            break;
 
-		// Create new transaction document
-		case "POST":
-			const transaction = await Transaction.create(req.body);
+        // Create new transaction document
+        case 'POST':
+            const transaction = await Transaction.create(req.body);
 
-			try {
-				res.status(201).json({
-					success: true,
-					msg: "Transaction made sucessfully.",
-					data: transaction,
-				});
-			} catch (err) {
-				res.status(400).json({ success: false, msg: err });
-			}
-			break;
+            try {
+                res.status(201).json({
+                    success: true,
+                    msg: 'Transaction made sucessfully.',
+                    data: transaction,
+                });
+            } catch (err) {
+                const missingFields = transactionErrorHandler(err);
+                res.status(400).json({
+                    success: false,
+                    msg: 'Transaction creation failed.',
+                    missingFields: missingFields,
+                });
+            }
+            break;
 
-		default:
-			res.status(500).json({ success: false, msg: "Route is not valid." });
-			break;
-	}
+        default:
+            res.status(500).json({ success: false, msg: 'Route is not valid.' });
+            break;
+    }
 }
 
 export default handler;
