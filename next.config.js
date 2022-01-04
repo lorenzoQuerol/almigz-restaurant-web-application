dee2c38e521ab31745da0f850ba2d87835e8f0ba
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-04 18:54:05 +0800

    Update next.config.js
    - Changed database to where documents are saved.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    reactStrictMode: true,
-    env: {
-        NEXTAUTH_URL: "http://localhost:3000",
-        SECRET: "koInOATRiQziM5VZgcyHZIh9jMJ5VrJjDHBg+XOHl0I=",
-        JWT_SECRET: "d81046d88b4f17f80c9e6a36ba3e53d6",
-        MONGODB_URI:
-            "mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/test?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
-        CONTENTFUL_SPACE_ID: "4vhelr5l3xmh",
-        CONTENTFUL_ACCESS_TOKEN: "zOgM8Qj5VXL9pjyqYuhbFvxvJE0vQzZRL_2Wt7uADZs",
-        CONTENTFUL_PREVIEW_ACCESS_TOKEN: "FeIT2KtX3Sy2GfGj2-b2q4yDISjkABqGTrMl5IqKpJw",
-    },
-    images: {
-        domains: ['images.ctfassets.net'],
-    },
+	reactStrictMode: true,
+	env: {
+		NEXTAUTH_URL: "http://localhost:3000",
+		SECRET: "koInOATRiQziM5VZgcyHZIh9jMJ5VrJjDHBg+XOHl0I=",
+		JWT_SECRET: "d81046d88b4f17f80c9e6a36ba3e53d6",
+		MONGODB_URI:
+			"mongodb+srv://ALMIGZ:Hc8uo7TYGAgkpZ8N@al-migz-db.rhiuv.mongodb.net/al-migz-restaurant-hauz?authSource=admin&replicaSet=atlas-ous3pp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+		CONTENTFUL_SPACE_ID: "4vhelr5l3xmh",
+		CONTENTFUL_ACCESS_TOKEN: "zOgM8Qj5VXL9pjyqYuhbFvxvJE0vQzZRL_2Wt7uADZs",
+		CONTENTFUL_PREVIEW_ACCESS_TOKEN: "FeIT2KtX3Sy2GfGj2-b2q4yDISjkABqGTrMl5IqKpJw",
+	},
+	images: {
+		domains: ["images.ctfassets.net"],
+	},
 };
