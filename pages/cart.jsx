5b1125a4fcdf98a6c70f50b1b41c00ec4cd38555
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-05 16:34:26 +0800

    Update cart.jsx
    - Refactored to new local storage operation functions

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -4,7 +4,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import { data } from "autoprefixer";
-import getCart from "@utils/foodCart/getCart";
+import getStorageValue from "@utils/localStorage/getStorageValue";
 
 var ctr = 0;
 
@@ -19,7 +19,7 @@ export default function Example() {
 
 	useEffect(() => {
 		// Perform localStorage action
-		var cart = getCart();
+		var cart = getStorageValue("foodCart");
 		setProducts(cart.data);
 		setSubtotal(cart.total + subtotal);
 		setTotal(cart.total + subtotal + delfee);
@@ -68,10 +68,10 @@ export default function Example() {
 							leaveFrom="opacity-100"
 							leaveTo="opacity-0"
 						>
-							<Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+							<Dialog.Overlay className="absolute inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
 						</Transition.Child>
 
-						<div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+						<div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
 							<Transition.Child
 								as={Fragment}
 								enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -82,14 +82,14 @@ export default function Example() {
 								leaveTo="translate-x-full"
 							>
 								<div className="w-screen max-w-md">
-									<div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-										<div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
+									<div className="flex flex-col h-full overflow-y-scroll bg-white shadow-xl">
+										<div className="flex-1 px-4 py-6 overflow-y-auto sm:px-6">
 											<div className="flex items-start justify-between">
 												<Dialog.Title className="text-lg font-medium text-gray-900">My Food Cart</Dialog.Title>
-												<div className="ml-3 h-7 flex items-center">
-													<button type="button" className="-m-2 p-2 text-gray-400 hover:text-gray-500" onClick={() => setOpen(false)}>
+												<div className="flex items-center ml-3 h-7">
+													<button type="button" className="p-2 -m-2 text-gray-400 hover:text-gray-500" onClick={() => setOpen(false)}>
 														<span className="sr-only">Close panel</span>
-														<XIcon className="h-6 w-6" aria-hidden="true" />
+														<XIcon className="w-6 h-6" aria-hidden="true" />
 													</button>
 												</div>
 											</div>
@@ -98,16 +98,16 @@ export default function Example() {
 												<div className="flow-root">
 													<ul role="list" className="-my-6 divide-y divide-gray-200">
 														{products.map((product) => (
-															<li key={product.id} className="py-6 flex">
-																<div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
+															<li key={product.id} className="flex py-6">
+																<div className="flex-shrink-0 w-24 h-24 overflow-hidden border border-gray-200 rounded-md">
 																	<img
 																		src={product.menuItem.productImagesCollection.items[0].url}
 																		//   alt={product.imageAlt}
-																		className="w-full h-full object-center object-cover"
+																		className="object-cover object-center w-full h-full"
 																	/>
 																</div>
 
-																<div className="ml-4 flex-1 flex flex-col">
+																<div className="flex flex-col flex-1 ml-4">
 																	<div>
 																		<div className="flex justify-between text-base font-medium text-gray-900">
 																			<h3>
@@ -117,11 +117,11 @@ export default function Example() {
 																		</div>
 																		<p className="mt-1 text-sm text-gray-500">P {product.menuItem.productPrice}</p>
 																	</div>
-																	<div className="flex-1 flex items-end justify-between text-sm">
+																	<div className="flex items-end justify-between flex-1 text-sm">
 																		<p className="text-gray-500">
 																			quantity:
 																			<input
-																				className="input font-black-100 mx-2 text-normal w-14 input-sm input-bordered rounded-md focus:ring-2 focus:ring-blue-300"
+																				className="mx-2 rounded-md input font-black-100 text-normal w-14 input-sm input-bordered focus:ring-2 focus:ring-blue-300"
 																				type="number"
 																				min="1"
 																				step="1"
@@ -153,12 +153,12 @@ export default function Example() {
 											</div>
 										</div>
 
-										<div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+										<div className="px-4 py-6 border-t border-gray-200 sm:px-6">
 											<div className="flex justify-between text-base font-medium text-gray-900">
 												<p>Subtotal</p>
 												<p>P{subtotal}</p>
 											</div>
-											<div className="flex justify-between text-base text-medium text-gray-500 my-3">
+											<div className="flex justify-between my-3 text-base text-gray-500 text-medium">
 												<p>Delivery Fee</p>
 												<p>P{delfee}</p>
 											</div>
@@ -170,15 +170,15 @@ export default function Example() {
 											<div className="mt-6">
 												<a
 													href="/checkout"
-													className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+													className="flex items-center justify-center px-6 py-3 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700"
 												>
 													Proceed to Checkout
 												</a>
 											</div>
-											<div className="mt-6 flex justify-center text-sm text-center text-gray-500">
+											<div className="flex justify-center mt-6 text-sm text-center text-gray-500">
 												<p>
 													or{" "}
-													<button type="button" className="text-green-600 font-medium hover:text-green-500" onClick={() => setOpen(false)}>
+													<button type="button" className="font-medium text-green-600 hover:text-green-500" onClick={() => setOpen(false)}>
 														Add more items<span aria-hidden="true"> &rarr;</span>
 													</button>
 												</p>
@@ -206,10 +206,10 @@ export default function Example() {
 							leaveFrom="opacity-100"
 							leaveTo="opacity-0"
 						>
-							<Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+							<Dialog.Overlay className="absolute inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
 						</Transition.Child>
 
-						<div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+						<div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
 							<Transition.Child
 								as={Fragment}
 								enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -220,14 +220,14 @@ export default function Example() {
 								leaveTo="translate-x-full"
 							>
 								<div className="w-screen max-w-md">
-									<div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-										<div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
+									<div className="flex flex-col h-full overflow-y-scroll bg-white shadow-xl">
+										<div className="flex-1 px-4 py-6 overflow-y-auto sm:px-6">
 											<div className="flex items-start justify-between">
 												<Dialog.Title className="text-lg font-medium text-gray-900">My Food Cart</Dialog.Title>
-												<div className="ml-3 h-7 flex items-center">
-													<button type="button" className="-m-2 p-2 text-gray-400 hover:text-gray-500" onClick={() => setOpen(false)}>
+												<div className="flex items-center ml-3 h-7">
+													<button type="button" className="p-2 -m-2 text-gray-400 hover:text-gray-500" onClick={() => setOpen(false)}>
 														<span className="sr-only">Close panel</span>
-														<XIcon className="h-6 w-6" aria-hidden="true" />
+														<XIcon className="w-6 h-6" aria-hidden="true" />
 													</button>
 												</div>
 											</div>
@@ -241,12 +241,12 @@ export default function Example() {
 											</div>
 										</div>
 
-										<div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+										<div className="px-4 py-6 border-t border-gray-200 sm:px-6">
 											<div className="flex justify-between text-base font-medium text-gray-900">
 												<p>Subtotal</p>
 												<p>P{subtotal}</p>
 											</div>
-											<div className="flex justify-between text-base text-medium text-gray-500 my-3">
+											<div className="flex justify-between my-3 text-base text-gray-500 text-medium">
 												<p>Delivery Fee</p>
 												<p>P{delfee}</p>
 											</div>
@@ -258,15 +258,15 @@ export default function Example() {
 											<div className="mt-6">
 												<a
 													href="/checkout"
-													className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+													className="flex items-center justify-center px-6 py-3 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700"
 												>
 													Proceed to Checkout
 												</a>
 											</div>
-											<div className="mt-6 flex justify-center text-sm text-center text-gray-500">
+											<div className="flex justify-center mt-6 text-sm text-center text-gray-500">
 												<p>
 													or{" "}
-													<button type="button" className="text-green-600 font-medium hover:text-green-500" onClick={() => setOpen(false)}>
+													<button type="button" className="font-medium text-green-600 hover:text-green-500" onClick={() => setOpen(false)}>
 														Add more items<span aria-hidden="true"> &rarr;</span>
 													</button>
 												</p>
