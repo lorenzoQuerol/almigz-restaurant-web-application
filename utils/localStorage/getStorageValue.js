75e04a3da4c5d2954b5afd7b599e24f8f23f9b28
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-05 16:32:17 +0800

    Added functions for local storage operations

---
+++ utils/localStorage/getStorageValue.js
@@ -0,0 +1,17 @@
+export default function getStorageValue(key) {
+	if (typeof window !== "undefined") {
+		if (key === "foodCart") {
+			const foodCart = JSON.parse(localStorage.getItem("foodCart"));
+
+			let total = 0;
+			if (foodCart) foodCart.forEach((item) => (total += item.menuItem.productPrice * item.quantity));
+
+			return { data: foodCart, total: total };
+		}
+
+		if (key === "transaction") {
+			const item = JSON.parse(localStorage.getItem(key));
+			return item;
+		}
+	}
+}
