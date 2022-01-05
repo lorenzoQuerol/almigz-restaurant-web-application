4370b75a45f640c000c94a0e33b09dc31d6414e2
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-05 17:40:53 +0800

    Update index.jsx

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -2,23 +2,16 @@
 // import Navbar from "../components/Navbar";
 // import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
-import useSWR from 'swr';
+import useSWR from "swr";
 import Link from "next/link";
 
-
 const fetcher = (url) => fetch(url).then((res) => res.json());
-// console.log(fetcher);
+
 export default function MenuPage() {
-    const { data, error } = useSWR('api/foodItems', fetcher);
-    const { data: session, status } = useSession();
-    console.log(data);
-    
-    
-    
-    
-    
-    
-    /*
+	const { data, error } = useSWR("api/foodItems", fetcher);
+	const { data: session, status } = useSession();
+
+	/*
       DESIGNERS: You can now use the session to conditionally render components for 
       authenticated or unauthenticated users. Session will return null if user is not
       logged in.
@@ -26,33 +19,38 @@ export default function MenuPage() {
       @status will return "authenticated" | "loading" | "unauthenticated"
     */
 
-    // if (session) {
-    //     return (
-    //         <>
-    //             <div>
-    //                 <h1>HOMEPAGE</h1>
-    //             </div>
-    //             Signed in as {session.user.name} <br />
-    //             <button onClick={signOut}>Sign out</button>
-    //         </>
-    //     );
-    // }
+	// if (session) {
+	//     return (
+	//         <>
+	//             <div>
+	//                 <h1>HOMEPAGE</h1>
+	//             </div>
+	//             Signed in as {session.user.name} <br />
+	//             <button onClick={signOut}>Sign out</button>
+	//         </>
+	//     );
+	// }
 
-    return (
-        <>
-            <div className="w-full text-center">
-                <h1 className="font-bold">Binalot</h1>
-                <Link href="/menu/pork-binagoongan">Pork Binagoongan</Link><br/>
-                <Link href="/menu/al-migz-sisig">Al Migz Sisig</Link><br/>
-                <h1 className="font-bold">House Specialties</h1>
-                <Link href="/menu/sizzling-sisig">Sizzling Sisig</Link><br/>
-                <h1 className="font-bold">Pancit in Boxes</h1>
-                <Link href="/menu/malabon-small">Malabon (Small)</Link><br/>
-                <Link href="/menu/malabon-medium">Malabon (Medium)</Link><br/>
-                <h1 className="font-bold">Desserts and Refreshments</h1>
-                <Link href="/menu/coke-1-5l">Coke 1.5L</Link><br/>
-            </div>
-            
-        </>
-    );
+	return (
+		<>
+			<div className="w-full text-center">
+				<h1 className="font-bold">Binalot</h1>
+				<Link href="/menu/pork-binagoongan">Pork Binagoongan</Link>
+				<br />
+				<Link href="/menu/al-migz-sisig">Al Migz Sisig</Link>
+				<br />
+				<h1 className="font-bold">House Specialties</h1>
+				<Link href="/menu/sizzling-sisig">Sizzling Sisig</Link>
+				<br />
+				<h1 className="font-bold">Pancit in Boxes</h1>
+				<Link href="/menu/malabon-small">Malabon (Small)</Link>
+				<br />
+				<Link href="/menu/malabon-medium">Malabon (Medium)</Link>
+				<br />
+				<h1 className="font-bold">Desserts and Refreshments</h1>
+				<Link href="/menu/coke-1-5l">Coke 1.5L</Link>
+				<br />
+			</div>
+		</>
+	);
 }
