78d5de5d68161ada606b0e94a93e87ba9157a451
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-07 22:43:51 +0800

    Update pushToCart.js

--- utils/foodCart/pushToCart.js
+++ utils/foodCart/pushToCart.js
@@ -1,14 +1,16 @@
 export default async function pushToCart(data) {
-  let foodCart = new Array();
-  if (localStorage.getItem('foodCart')) foodCart = JSON.parse(localStorage.getItem('foodCart'));
+	if (typeof window !== "undefined") {
+		let foodCart = new Array();
+		if (localStorage.getItem("foodCart")) foodCart = JSON.parse(localStorage.getItem("foodCart"));
 
-  for (var i = 0; i < foodCart.length; i++) {
-    if (foodCart[i].menuItem.productName == data.productName) {
-      foodCart[i].menuItem.quantity = foodCart[i].menuItem.quantity + data.quantity;
-      break;
-    }
-  }
+		for (var i = 0; i < foodCart.length; i++) {
+			if (foodCart[i].menuItem.productName == data.productName) {
+				foodCart[i].menuItem.quantity = foodCart[i].menuItem.quantity + data.quantity;
+				break;
+			}
+		}
 
-  if (i == foodCart.length) foodCart.push(data);
-  localStorage.setItem('foodCart', JSON.stringify(foodCart));
+		if (i == foodCart.length) foodCart.push(data);
+		localStorage.setItem("foodCart", JSON.stringify(foodCart));
+	}
 }
