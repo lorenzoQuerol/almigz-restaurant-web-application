0eda47121b72c98080032ebcb8351117239e6bdc
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-13 01:38:18 +0800

    Upgraded to Tailwind v3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,15 @@
-
-
 module.exports = {
-    mode: 'jit',
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                'rale': ['Raleway', 'sans-serif'],
-              },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require('daisyui'),
-    ],
-    daisyui: {
-        themes: [
-            'lofi',
-        ],
-    }
+	mode: "jit",
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				rale: ["Raleway", "sans-serif"],
+			},
+		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["lofi"],
+	},
 };
