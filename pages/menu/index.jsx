b5cfcee852343089672b5c722094149308c2c639
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-17 11:00:19 +0800

    Responsive menu page

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -1,31 +1,53 @@
 import useSWR from "swr";
+import { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
 import Link from "next/link";
+import Image from "next/image";
+
+import MenuHeader from "@components/MenuHeader";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function MenuPage() {
 	const { data, error } = useSWR("/api/foodItems", fetcher);
+	const [foodItems, setFoodItems] = useState([]);
+	const [activeCategory, setActiveCategory] = useState("Show all");
+
+	useEffect(() => {
+		if (data) setFoodItems(data.data);
+	}, [data]);
+
+	let filtered = foodItems.filter((foodItem) => foodItem.category === activeCategory);
+	if (filtered.length === 0) filtered = foodItems;
 
 	return (
-		<>
-			<div className="w-full text-center">
-				<h1 className="font-bold">Binalot</h1>
-				<Link href="/menu/pork-binagoongan">Pork Binagoongan</Link>
-				<br />
-				<Link href="/menu/al-migz-sisig">Al Migz Sisig</Link>
-				<br />
-				<h1 className="font-bold">House Specialties</h1>
-				<Link href="/menu/sizzling-sisig">Sizzling Sisig</Link>
-				<br />
-				<h1 className="font-bold">Pancit in Boxes</h1>
-				<Link href="/menu/malabon-small">Malabon (Small)</Link>
-				<br />
-				<Link href="/menu/malabon-medium">Malabon (Medium)</Link>
-				<br />
-				<h1 className="font-bold">Desserts and Refreshments</h1>
-				<Link href="/menu/coke-1-5l">Coke 1.5L</Link>
-				<br />
+		<div className="text-gray-900 font-rale">
+			<MenuHeader activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+			<div className="container px-5 py-1 mx-auto">
+				<div className="h-1 mb-6 overflow-hidden bg-gray-200 rounded">
+					<div className="w-24 h-full bg-green-700"></div>
+				</div>
+				<div className="flex flex-wrap -m-4">
+					{filtered.map((foodItem) => {
+						return (
+							<Link href={`/menu/${encodeURIComponent(foodItem.slug)}`}>
+								<div className="w-full p-4 transition-transform cursor-pointer hover:scale-105 lg:w-1/4 md:w-1/2" key={foodItem.slug}>
+									<div className="pb-2 rounded bg-gray-50 drop-shadow-md">
+										<a className="relative block overflow-hidden rounded-t">
+											<Image width={420} height={260} src={foodItem.productImagesCollection.items[0].url} />
+										</a>
+										<div className="mt-3 ml-2">
+											<h3 className="mb-1 text-xs font-bold tracking-widest text-green-700">{foodItem.category}</h3>
+											<h2 className="text-lg font-semibold">{foodItem.productName}</h2>
+											<p className="mt-1">₱{foodItem.productPrice}</p>
+										</div>
+									</div>
+								</div>
+							</Link>
+						);
+					})}
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
