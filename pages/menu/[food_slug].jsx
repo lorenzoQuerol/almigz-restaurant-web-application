1b0d90f23546544c7fc2238f895b93a03d475692
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-17 11:00:03 +0800

    Responsive food item page

--- pages/menu/[food_slug].jsx
+++ pages/menu/[food_slug].jsx
@@ -25,8 +25,7 @@ export default function FoodItemPage() {
 		if (Number.isInteger(num) && num > 0 && num < 10000) {
 			setTotal(data.data.productPrice * num);
 			setquantity(num);
-		}
-		else {
+		} else {
 			setTotal(data.data.productPrice);
 			setquantity(1);
 		}
@@ -43,11 +42,15 @@ export default function FoodItemPage() {
 			};
 
 			pushToCart(item);
-			setOpen(!open);
+			handleOpen();
 		} else {
 			alert("Please login before adding to cart.");
 			router.push("/auth/signIn");
 		}
+	};
+
+	const handleOpen = () => {
+		setOpen(!open);
 	};
 
 	if (!data) return <h1>Loading...</h1>;
@@ -67,30 +70,60 @@ export default function FoodItemPage() {
 		const pic = data.data.productImagesCollection.items[0].url;
 
 		return (
-			<>
-				<div onLoad={(e) => setTotal(data.data.productPrice)} className="flex-col w-1/2 p-10 text-center rounded-md shadow-2xl shadow-yellow-400">
-					<h1 className="text-3xl font-bold text-black font-rale">{data.data.productName}</h1>
-					<h1>{data.data.category}</h1>
-					<Image src={pic} layout="intrinsic" className="bg-gray-100 shadow-2xl shadow-black-400" width="300" height="200" />
-					<h1 className="mt-8">{data.data.productDescription}</h1>
-					<h1 className="pb-3 mt-12 text-2xl font-bold text-green-700">P {total}</h1>
-					<form onSubmit={addToCart} className="flex-col">
-						<input
-							className="w-24 p-5 pr-2 mr-2 text-xl rounded-md input font-black-100 input-sm input-bordered focus:ring-2 focus:ring-blue-300"
-							type="number"
-							min="1"
-							step="1"
-							max="9999"
-							placeholder="1"
-							onLoad={(e) => setTotal(data.data.productPrice)}
-							onChange={(e) => updateTotal(e.target.value)}
-						></input>
-						pc
-						<button className="p-4 m-5 font-normal text-white bg-green-500 rounded-lg pl-7 pr-7 hover:font-medium hover:bg-green-300">Add to Cart</button>
-					</form>
+			<div className="container px-5 py-8 mx-auto">
+				<div onLoad={(e) => setTotal(data.data.productPrice)} className="overflow-hidden text-gray-600 body-font font-rale">
+					<div className="mx-auto mb-8 text-md breadcrumbs xl:w-4/5">
+						<ul>
+							<li>
+								<Link href="/menu">
+									<a>Menu</a>
+								</Link>
+							</li>
+							<li>
+								<Link href={`/menu/${router.query.food_slug}`}>
+									<a>{data.data.productName}</a>
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="flex flex-wrap justify-center mx-auto xl:w-4/5">
+						<Image className="object-cover object-center w-full h-64 rounded lg:w-1/2 lg:h-auto" width={400} height={400} layout="fixed" src={pic} />
+
+						<div className="w-full mt-6 lg:w-1/2 lg:pl-10 lg:py-6 lg:mt-0">
+							<h2 className="text-sm tracking-widest text-gray-500 title-font">{data.data.category.toUpperCase()}</h2>
+							<h1 className="mb-1 text-3xl font-semibold text-gray-900 title-font">{data.data.productName}</h1>
+							<div className="flex mb-4"></div>
+							<p className="leading-relaxed">{data.data.productDescription}</p>
+							<form onSubmit={addToCart}>
+								<div className="flex items-center pb-5 mt-6 mb-5 border-b-2 border-gray-100">
+									<div className="flex items-center">
+										<span className="mr-3">Quantity:</span>
+										<div className="relative">
+											<input
+												className="py-2 pl-3 pr-2 text-base border border-gray-300 rounded appearance-none focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-green-700"
+												type="number"
+												min="1"
+												step="1"
+												max="9999"
+												placeholder="1"
+												onLoad={(e) => setTotal(data.data.productPrice)}
+												onChange={(e) => updateTotal(e.target.value)}
+											></input>
+										</div>
+									</div>
+								</div>
+								<div className="flex">
+									<span className="text-2xl font-medium text-gray-900 title-font">₱{total}</span>
+									<button className="flex px-6 py-2 ml-auto font-semibold text-white transition-colors bg-green-700 border-0 rounded focus:outline-none hover:bg-green-600">
+										Add to Cart
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
 				</div>
-				<Cart open={open} setOpen={setOpen} />
-			</>
+				<Cart open={open} handleOpen={handleOpen} />
+			</div>
 		);
 	}
 }
