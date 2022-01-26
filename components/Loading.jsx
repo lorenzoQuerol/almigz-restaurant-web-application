206607e20595119636a02dfa049aa36537df427c
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-26 20:33:54 +0800

    Create Loading.jsx

---
+++ components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+	return (
+		<div class="flex items-center justify-center h-full">
+			<div class="w-10 h-10 border-4 border-t-transparent border-green-700 border-solid rounded-full animate-spin"></div>
+		</div>
+	);
+};
+
+export default Loading;
