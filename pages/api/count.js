e9a5d00ec1a49705e07d68cfb716aa3287e80f21
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-31 01:23:29 +0800

    Update count.js

--- pages/api/count.js
+++ pages/api/count.js
@@ -2,12 +2,13 @@ import { getSession } from "next-auth/react";
 
 import createConnection from "@utils/mongoDBConnection";
 import Transaction from "@models/TransactionModel";
+import User from "@models/UserModel";
 
 async function handler(req, res) {
 	await createConnection();
 	const session = await getSession({ req });
 	const {
-		query: { email },
+		query: { filter },
 		method,
 	} = req;
 
@@ -16,8 +17,18 @@ async function handler(req, res) {
 		case "GET":
 			if (session) {
 				try {
-					// Get count of transactions in the database
-					const count = await Transaction.count({});
+					let count;
+					// Get count of ALL transactions in the database
+					if (filter === "transactions") count = await Transaction.count({});
+
+					// Get count of COMPLETE transactions in the database
+					if (filter === "complete") count = await Transaction.count({ orderStatus: 4 });
+
+					// Get count of ACTIVE transactions in the database
+					if (filter === "active") count = await Transaction.count({ orderStatus: { $gt: 0, $lt: 4 } });
+
+					// Get count of users in database
+					if (filter === "users") count = await User.count({});
 
 					res.status(200).json({ success: true, message: "Successful query", count });
 				} catch (err) {
