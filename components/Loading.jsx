95d81b34f6a63faedd537e44bbee8cd3366b1203
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-01-31 12:15:45 +0800

    Update Loading.jsx

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
 	return (
-		<div class="flex items-center justify-center h-full">
-			<div class="w-10 h-10 border-4 border-t-transparent border-green-700 border-solid rounded-full animate-spin"></div>
+		<div className="flex items-center justify-center h-full">
+			<div className="w-10 h-10 border-4 border-green-700 border-solid rounded-full border-t-transparent animate-spin"></div>
 		</div>
 	);
 };
