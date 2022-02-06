0a842bded3bfd41ae483f995c546cc93c22a656d
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-02-06 14:41:17 +0800

    Revised UI

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -40,7 +40,7 @@ const Cart = ({ open, handleOpen }) => {
 		setTotal(temp);
 
 		cart.products = products;
-		cart.total = total;
+		cart.total = temp;
 		window.localStorage.setItem("foodCart", JSON.stringify(cart));
 
 		if (products.length === 0) {
@@ -126,9 +126,9 @@ const Cart = ({ open, handleOpen }) => {
 																			<h3>
 																				<a href="#">{product.menuItem.productName}</a>
 																			</h3>
-																			<p className="ml-4">P {product.menuItem.productPrice * product.quantity}</p>
+																			<p className="w-1/4 ml-4 text-right">₱ {product.menuItem.productPrice * product.quantity}</p>
 																		</div>
-																		<p className="mt-1 text-sm text-gray-500">P {product.menuItem.productPrice}</p>
+																		<p className="mt-1 text-sm text-gray-500">₱ {product.menuItem.productPrice}</p>
 																	</div>
 																	<div className="flex items-end justify-between flex-1 text-sm">
 																		<p className="text-gray-500">
@@ -169,13 +169,13 @@ const Cart = ({ open, handleOpen }) => {
 										<div className="px-4 py-6 border-t border-gray-200 sm:px-6">
 											<div className="flex justify-between text-base font-medium text-gray-800">
 												<p>Total</p>
-												<p>P{total}</p>
+												<p>₱{total}</p>
 											</div>
 											{/* <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p> */}
 											<div className="mt-6">
 												<a
 													href="/checkout"
-													className="flex items-center justify-center px-6 py-3 text-base font-normal text-white transition-colors bg-green-700 border rounded shadow-sm hover:bg-green-600"
+													className="flex items-center justify-center px-6 py-3 text-base font-medium text-white transition-colors bg-green-700 border rounded shadow-sm hover:bg-green-600"
 												>
 													Proceed to Checkout
 												</a>
