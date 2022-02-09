31f7c9b8a752cd0948bf9aa67e754b201f0eb30b
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2022-02-10 00:26:46 +0800

    Update ViewUserDialog.jsx

--- components/ViewUserDialog.jsx
+++ components/ViewUserDialog.jsx
@@ -1,43 +1,44 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Dialog, Transition, Switch } from "@headlessui/react";
 import axios from "axios";
 
 const ViewUserDialog = ({ user, refetch, openViewUser, setOpenViewUser }) => {
 	const [toggleAdmin, setToggleAdmin] = useState(user.isAdmin);
-
 	const handleToggleAdmin = () => {
 		setToggleAdmin(!toggleAdmin);
 	};
 
-	const handleAdminChange = async () => {
-		setOpenViewUser(false);
+	useEffect(() => {
+		setToggleAdmin(user.isAdmin);
+	}, [user]);
 
-		setTimeout(async () => {
+	const handleAdminChange = async () => {
+		if (toggleAdmin === user.isAdmin) {
+			setOpenViewUser(false);
+		} else {
 			user.isAdmin = toggleAdmin;
 			const response = await axios.put(`/api/users/${user.email}`, user);
 			if (response.status < 300) refetch();
-		}, 1500);
-	};
 
-	const handleCloseViewUser = () => {
-		setOpenViewUser(false);
+			setOpenViewUser(false);
+		}
 	};
 
 	return (
 		<>
 			<Transition appear show={openViewUser} as={Fragment}>
-				<Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto text-slate-900 font-rale" onClose={handleAdminChange}>
+				<Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto text-gray-800 font-rale" onClose={handleAdminChange}>
 					<div className="min-h-screen px-4 text-center">
 						<Transition.Child
 							as={Fragment}
 							enter="ease-out duration-200"
-							enterFrom="opacity-10"
+							enterFrom="opacity-0"
 							enterTo="opacity-100"
 							leave="ease-in duration-200"
-							leaveFrom="opacity-50"
+							leaveFrom="opacity-100"
 							leaveTo="opacity-0"
 						>
-							<Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+							<Dialog.Overlay className="fixed inset-0 bg-gray-800 opacity-40" />
 						</Transition.Child>
 
 						{/* This element is to trick the browser into centering the modal contents. */}
@@ -54,29 +55,26 @@ const ViewUserDialog = ({ user, refetch, openViewUser, setOpenViewUser }) => {
 							leaveTo="opacity-0 scale-95"
 						>
 							<div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-								<Dialog.Title as="h3" className="text-lg font-bold leading-6 text-gray-900">
+								<Dialog.Title as="h3" className="text-lg font-semibold leading-6 text-gray-800">
 									User Details
 								</Dialog.Title>
 								<div className="mt-2">
-									{/* <p className="text-sm text-gray-500">
-										Are you sure you want to delete your account? All of your data will be permanently removed. This action cannot be undone.
-									</p> */}
-									<div className="font-bold">Full Name</div>
+									<div className="font-semibold">Full Name</div>
 									<div className="pb-2 mb-2 border-b-2">
 										{user.firstName} {user.lastName}
 									</div>
-									<div className="font-bold">Email</div>
+									<div className="font-semibold">Email</div>
 									<div className="pb-2 mb-2 border-b-2">{user.email}</div>
-									<div className="font-bold">Address</div>
+									<div className="font-semibold">Address</div>
 									<div className="pb-2 mb-2 border-b-2">{user.address}</div>
-									<div className="font-bold">Contact Numbers</div>
+									<div className="font-semibold">Contact Numbers</div>
 									<div>{user.contact1}</div>
 									<div>{user.contact2}</div>
 								</div>
 
 								<div className="flex justify-end mt-4 space-x-4">
 									<Switch.Group>
-										<Switch.Label className="self-center font-bold">Give Admin Access</Switch.Label>
+										<Switch.Label className="self-center w-full text-xs font-bold sm:text-sm">Give Admin Access</Switch.Label>
 										<Switch
 											checked={toggleAdmin}
 											onChange={handleToggleAdmin}
@@ -93,10 +91,10 @@ const ViewUserDialog = ({ user, refetch, openViewUser, setOpenViewUser }) => {
 
 									<button
 										type="button"
-										className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white transition-colors bg-green-700 border border-transparent rounded-md hover:bg-green-600"
-										onClick={handleCloseViewUser}
+										className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white transition-colors bg-green-700 border border-transparent rounded-md sm:w-full hover:bg-green-600"
+										onClick={handleAdminChange}
 									>
-										Back
+										{toggleAdmin === user.isAdmin ? "Back" : "Update"}
 									</button>
 								</div>
 							</div>
