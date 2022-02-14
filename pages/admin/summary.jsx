3c0f940118d92cd9973bca1e9e724e7423fe63d3
renjirong <70014388+renjirong@users.noreply.github.com>
2022-02-15 01:32:57 +0800

    Added download function in summary

--- pages/admin/summary.jsx
+++ pages/admin/summary.jsx
@@ -1,29 +1,41 @@
+import downloadSummary from "@utils/downloadSummary";
+import useAxios from "axios-hooks";
+import React, { useState,useEffect } from "react";
+
+
+
 export default function Summary() {
-	import downloadSummary from "@utils/downloadSummary";
+	
 
 	const [{ data, loading, error }, refetch] = useAxios({
-		url: `${process.env.NEXTAUTH_URL}/api/transactions`,
-		params: {
-			limit: limit,
-			offset: (page - 1) * limit,
-			filter: watch("filter") == "All" ? null : Number(watch("filter")),
-		},
+		url: `${process.env.NEXTAUTH_URL}/api/transactions`
 	});
 
+	function print(){
+		downloadSummary(data.transactions)
+	}
+	
 	// ANCHOR Refetch for pagination
 	useEffect(() => {
 		setTimeout(() => {
 			refetch();
-			setLastUpdate(new Date());
 		}, 60000);
-
-		if (data) {
-			setTransactions(data.transactions);
-			
-		}
 	}, [data]);
 
-	return <h1>hello world!</h1>;
+	return (
+		<div className="flex self-center justify-between text-white lg:space-x-10 lg:justify-center">
+			<div className="flex flex-col mt-3 w-52">
+				<button
+					type="button"
+					onClick={print}
+					className={`px-8 py-2 text-sm text-white transition duration-150 ease-in-out bg-green-700 border rounded hover:bg-green-600 focus:outline-none`}
+				>
+					Download Summary
+				</button>
+			</div>			
+		</div>
+		
+	);
 }
 
 Summary.layout = "admin";
--- utils/downloadSummary.js
+++ utils/downloadSummary.js
@@ -1,6 +1,7 @@
 
 
 function downloadCSV (csv){
+    
     // Get date and time
     const today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -16,13 +17,38 @@ function downloadCSV (csv){
     tag.download = "SummaryReport_" + dateTime + ".csv";
     tag.click()
 }
+
+
 export default function downloadSummary(data){
-    var json = []
-    json.push(data.transaction)
     
+    const status = ["INCOMING", "PROCESSED", "IN PREPARATION", "READY FOR PICKUP/DELIVERY", "COMPLETED ORDER", "CANCELLED ORDER"];
+   
+    var json = [];
+   
+    
+    
+    for (var i = 0; i < data.length; i++){
+        for (var j = 0; j < data[i].order.length; j++){
+            var filter ={
+                "Full Name":data[i].fullName,
+                "Contact Number":data[i].contactNum[0],
+                "Branch":data[i].branch,
+                "Invoice Number": data[i].invoiceNum,
+                "Status" : status[data[i].orderStatus],
+                "Remarks":data[i].reasonForCancel,
+                "Total Price":data[i].order[j].quantity * data[i].order[j].menuItem.productPrice,
+                "Payment Method": data[i].payMethod,
+                "Date Created":data[i].dateCreated,
+                "Product Name":data[i].order[j].menuItem.productName,
+                "Qty":data[i].order[j].quantity
+            }
+            json.push(filter);
+        }
+    }
+
+    
+
     var fields = Object.keys(json[0])
-    
-    
     var replacer = function(key, value) { return value === null ? '' : value } 
 
     var csv = json.map(function(row){
@@ -33,7 +59,7 @@ export default function downloadSummary(data){
     csv.unshift(fields.join(',')) // add header column
     csv = csv.join('\r\n');
 
-    console.log(csv)
+    // console.log(csv)
     // console.log(data.transaction.dateCreated)
     // console.log(data.transaction.branch)
     // console.log(data.transaction.deliveryTime)
