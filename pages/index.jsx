326d064ee4e71ca85d4c0849148a6a8d88cb877b
gretvm <80465081+gretvm@users.noreply.github.com>
2022-02-19 12:23:35 +0800

    Homepage Carousel

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,42 +25,73 @@ export async function getServerSideProps(context) {
 export default function Home(session) {
 	const { data, error } = useSWR("/api/home", fetcher);
 
-	if (!data) return <h1 className="h-screen">Loading...</h1>;
+	if (!data) return <h1 className="h-full">Loading...</h1>;
 
-	const banners = data.homepageItems;
+	const banners = data.homepageItems[0];
+
+	console.log(banners.announcementsCollection.items[2]);
 	return (
 		<>
-			<div className="flex flex-col items-center justify-center w-full">
-				{/* {banners.map((pics) => (
-					<div className="flex flex-col">
-						{pics.announcementsCollection.items.map((pic, index) => (
-							<a key={index} className="">
-								<Image className="" src={pic.url} alt="announcement" width={1260} height={825} />
-							</a>
+			<div className="flex flex-col items-center w-full">
+				<div id="banners" className="carousel slide relative" data-bs-ride="carousel" data-bs-pause="hover">
+					<div className="carousel-inner relative w-full overflow-hidden">
+						{banners.announcementsCollection.items.map((pic, i) => (
+						<div className={`${i == 1 ? "active" : ""} carousel-item relative float-left w-full`}>
+							<Image
+								src={pic.url}
+								width={3500} height={1500}
+								className="block"
+								alt="..."
+							/>
+						</div>
 						))}
+						<div className="block absolute text-center w-full h-full bg-gradient-to-b from-white/0 via-white/0 to-yellow-600/90">
+							<div className="carousel-caption absolute bottom-2 text-center p-0 pb-2 md:pb-5">
+								<h1 className="text-black font-rale font-bold mb-1 md:mb-2 lg:mb-4 lg:mb-6 text-sm md:text-3xl lg:text-5xl 2xl:text-6xl tracking-wide" >{banners.label}</h1>
+								<a
+									className="inline-block px-4 py-2 md:px-7 md:py-3 border md:border-2 border-white text-xs lg:text-lg 2xl:text-3xl font-sans  bg-black/60 hover:backdrop-none hover:border-black hover:text-black rounded text-white font-medium leading-snug uppercase hover:bg-black hover:bg-opacity-5"
+									href="/menu"
+									role="button"
+								>View Menu</a>
+							</div>
+						</div>
 					</div>
-				))} */}
+					<button
+						className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+						type="button"
+						data-bs-target="#banners"
+						data-bs-slide="prev"
+					>
+						<span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+						<span className="visually-hidden">Previous</span>
+					</button>
+					<button
+						className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+						type="button"
+						data-bs-target="#banners"
+						data-bs-slide="next"
+					>
+						<span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+						<span className="visually-hidden">Next</span>
+					</button>
+				</div>
 
-				{/* <div className="sticky flex items-center self-center justify-center w-full p-4 mb-2 bg-black opacity-75 bottom-36">
-					<h1 className="mr-4 italic font-light text-gray-400">Order now: </h1>
-					<Link href="/menu">
-						<a className="p-2 m-2 text-xl font-semibold text-yellow-600 border-2 border-yellow-500 font-rale rounded-btn hover:text-white hover:bg-yellow-500 hover:border-none">
-							VIEW MENU
+				<div className="flex flex-wrap mt-6 items-center self-center justify-center text-center h-full">
+					<h1 className="p-2 text-sm md:text-xl font-bold text-black font-rale w-full">Also available in: </h1>
+					<div className="rounded p-2 m-4 bg-white border-2 border-gray-200 hover:border-green-600 h-14 w-50" data-tip="https://food.grab.com/ph/en/">
+					<Link href="https://food.grab.com/ph/en/" target="_blank">
+						<a target="_blank" rel="noopener noreferrer">
+							<Image src="/grab.png" className="block m-2" alt="grabfood" width={180} height={30} />
 						</a>
 					</Link>
-			</div> */}
-				<div className="flex flex-wrap items-center self-center justify-center mt-10">
-					<h1 className="p-2 text-xl font-bold text-black font-rale">Also available in: </h1>
-					<a href="https://food.grab.com/ph/en/" target="_blank">
-						<div className="p-2 m-4 bg-white tooltip hover:border-2 hover:border-green-600 h-14 w-50" data-tip="https://food.grab.com/ph/en/">
-							<Image src="/grab.png" className="m-2 bg-white btn hover:bg-white" alt="grabfood" width={180} height={30} />
-						</div>
-					</a>
-					<a href="https://www.foodpanda.ph/" target="_blank">
-						<div className="p-2 m-4 bg-white tooltip hover:border-2 hover:border-pink-600 h-14 w-65" data-tip="https://www.foodpanda.ph/">
-							<Image src="/panda.png" className="m-2 bg-white btn hover:bg-white" alt="foodpanda" width={200} height={40} />
-						</div>
-					</a>
+					</div>
+					<div className="rounded p-2 m-4 bg-white border-2 border-gray-200 hover:border-pink-600 h-14 w-50" data-tip="https://food.grab.com/ph/en/">
+					<Link href="https://www.foodpanda.ph/" target="_blank">
+						<a target="_blank" rel="noopener noreferrer">
+							<Image src="/panda.png" className="block m-2" alt="grabfood" width={200} height={40} />
+						</a>
+					</Link>
+					</div>
 				</div>
 			</div>
 		</>
